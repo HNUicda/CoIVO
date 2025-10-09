2a23ea1cb2ddf3e41ad2d58010e28db3be3aa2df
Ming <hnuicda@gmail.com>
2025-10-09 11:06:51 +0800

    delete

--- PDutils/DEFINE.py
+++
@@ -1,9 +0,0 @@
-START_FRAME = 20
-# DATA_ROOT = '../../data/seq/seq4'
-DATA_ROOT = '../../data/CSD3/train'
-
-# seq3, seq4
-START_FRAME_SEQ3 = 40
-START_FRAME_SEQ4 = 20
-MIN_DEPTH = 0.5
-MAX_DEPTH = 100
--- dataloaders/__init__.py
+++
@@ -1,4 +0,0 @@
-from .rgbm_Dataloader import aDataset
-from .test_Dataloader import aDataset as testDataset
-from .exr_dataloader_single import aDataset as exrDataset_sin
-from .exr_dataloader_mono import aDataset as exrDataset
